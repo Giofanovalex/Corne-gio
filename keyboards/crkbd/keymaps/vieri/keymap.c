d21ecffb8a470b25b22f0986c677543dfb4cbadf
Giofanov <vierihery@gmail.com>
2026-06-09 22:54:11 +0700

    feat: change right shift mod-tap to simple shift key

--- keyboards/crkbd/keymaps/vieri/keymap.c
+++ keyboards/crkbd/keymaps/vieri/keymap.c
@@ -21,13 +21,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_3x6_3(
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
         KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,        KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                           KC_LALT, MO(_LOWER), KC_ENT,            KC_SPC,  MO(_RAISE), KC_RGUI
     ),
     [_LOWER] = LAYOUT_split_3x6_3(
         KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
         KC_TRNS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_GRV,  KC_TILD,
-        KC_TRNS, KC_ESC,  KC_LGUI, KC_LALT, KC_CAPS, KC_DQUO,     KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, KC_PSCR, RSFT_T(KC_SPC),
+        KC_TRNS, KC_ESC,  KC_LGUI, KC_LALT, KC_CAPS, KC_DQUO,     KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, KC_PSCR, KC_TRNS,
                           KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [_RAISE] = LAYOUT_split_3x6_3(
